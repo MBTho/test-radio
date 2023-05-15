bf721b24ed25fe24935df5843e7892b6156134d0
MBTho <132676870+MBTho@users.noreply.github.com>
2023-05-15 09:04:02 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,15 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    basic.showString("" + (receivedNumber))
-})
 input.onButtonPressed(Button.A, function () {
     radio.sendString("A")
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendString("")
 })
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
 input.onButtonPressed(Button.B, function () {
     radio.sendString("B")
 })
-radio.setGroup(1)
+radio.setFrequencyBand(70)
+radio.setGroup(70)
+radio.setTransmitPower(7)
